a04fc48111964ae6591b196f2050ca0e7bccb5bd
Celerati <ortigozagabriel842@gmail.com>
2025-04-16 19:06:23 -0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -33,7 +33,7 @@
         double_perk_points_gain: 0,
         instant_dark_matter: 0,
         keep_dark_mater_skills: 0,
-        hyper_speed: 1,
+        hyper_speed: 0,
         both_dark_mater_skills: 0,
         evil_booster: 0,
         more_perk_points : 0
@@ -196,7 +196,7 @@ const skillBaseData = {
     "Concentration": { name: "Concentration", maxXp: 100, heroxp: 36, effect: 0.01, description: "Skill XP" },
     "Productivity": { name: "Productivity", maxXp: 100, heroxp: 37, effect: 0.01, description: "Hero XP" },
     "Bargaining": { name: "Bargaining", maxXp: 100, heroxp: 38, effect: -0.01, description: "Reduced Expenses" },
-    "Meditation": { name: "Meditation", maxXp: 100, heroxp: 39, effect: 0.01, description: "Happiness" },
+    "Meditation": { name: "Meditation", maxXp: 100, heroxp: 39, effect: 1, description: "Happiness" },
 
     "Strength": { name: "Strength", maxXp: 100, heroxp: 40, effect: 0.01, description: "Military Pay" },
     "Battle Tactics": { name: "Battle Tactics", maxXp: 100, heroxp: 41, effect: 0.01, description: "Military XP" },
@@ -204,7 +204,7 @@ const skillBaseData = {
 
     "Mana Control": { name: "Mana Control", maxXp: 100, heroxp: 46, effect: 0.01, description: "T.A.A. XP" },
     "Life Essence": { name: "Life Essence", maxXp: 100, heroxp: 82, effect: 0.01, description: "Longer Lifespan" },
-    "Time Warping": { name: "Time Warping", maxXp: 100, heroxp: 82, effect: 0.01, description: "Gamespeed" },
+    "Time Warping": { name: "Time Warping", maxXp: 100, heroxp: 82, effect: 0.1, description: "Gamespeed" },
     "Astral Body": { name: "Astral Body", maxXp: 100, heroxp: 100, effect: 0.0035, description: "Longer lifespan" },
     "Temporal Dimension": { name: "Temporal Dimension", maxXp: 100, heroxp: 115, effect: 0.006, description: "Gamespeed" },
     "All Seeing Eye": { name: "All Seeing Eye", maxXp: 100, heroxp: 120, effect: 0.0027, description: "T.A.A Pay" },
